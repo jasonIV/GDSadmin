15196de7ce536c26e3e357e20b1041a174fedacc
Aung Ko Ko Thet <aungkokothet@gmail.com>
2020-04-24 17:38:06 +0630

    Second Commit

--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -16,7 +16,7 @@ class Dashboard extends Component{
                         <div className="balance inn-content">
                             <ul className="list">
                                 <li>
-                                    <p>Balance  <i className="float-right"> 20000 ks</i></p>
+                                    <p>Balance  <i className="float-right"> 30000 ks</i></p>
                                 </li>
                             </ul>
                             <div className="app"><Link to="/partnerApp"><AiFillPlusCircle/></Link></div>
