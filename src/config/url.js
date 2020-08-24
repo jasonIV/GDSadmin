0b642cf2c78dfdd1e95d0c45d3ec2b632115c946
jasonIV <46783042+jasonIV@users.noreply.github.com>
2020-08-25 00:40:40 +0630

    Update url.js

--- src/config/url.js
+++ src/config/url.js
@@ -1,2 +1,1 @@
-export const firebaseUrl = "https://us-central1-gdsprototypeapi-1968a.cloudfunctions.net/app/api/"
-export const awsUrl = "https://efi2torz90.execute-api.ap-southeast-1.amazonaws.com/staging/"
+export const awsUrl = "https://efi2torz90.execute-api.ap-southeast-1.amazonaws.com/production/"
